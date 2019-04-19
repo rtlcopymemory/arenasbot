0e41ee72b8d84a042817d054b565422085438abf
StackWolfed <stackwolfed@protonmail.com>
2019-04-19 20:58:57 +0200

    query not working

--- main.py
+++ main.py
@@ -12,27 +12,29 @@ conn.execute("CREATE TABLE IF NOT EXISTS arenas (id INTEGER PRIMARY KEY AUTOINCR
 prefix = 'w.'
 
 def checkDatabaseForDelete():
-    timeUp = dt.datetime.now() - dt.timedelta(hours=1, minutes=0)
-    curr = conn.cursor()
-    curr.execute("SELECT * FROM arenas WHERE time < ?", timeUp)
-    results = curr.fetchall()
-    print(results)
-    threading.Timer(20.0, checkDatabaseForDelete).start()
+  conn2 = sqlite3.connect('example.db', detect_types=sqlite3.PARSE_DECLTYPES)
+  timeUp = dt.datetime.now() - dt.timedelta(hours=0, minutes=1)
+  print(timeUp)
+  curr = conn2.cursor()
+  curr.execute("SELECT * FROM arenas WHERE time < ?", (timeUp, ))
+  results = curr.fetchall()
+  print(results)
+  threading.Timer(20.0, checkDatabaseForDelete).start()
 
 class MyClient(discord.Client):
-    async def on_ready(self):
-        print('Logged on as {0}!'.format(self.user))
-        checkDatabaseForDelete()
+  async def on_ready(self):
+    print('Logged on as {0}!'.format(self.user))
+    checkDatabaseForDelete()
 
-    async def on_message(self, message):
-        if (message.content.lower().startswith(prefix + 'startarena')):
-          print("Lo sbatti che ho")
-          curr = conn.cursor()
-          curr.execute(u'''
-          INSERT INTO arenas (author)
-          VALUES ('{}')
-          '''.format(message.author))
-          print("Ok, inserted {} into database".format(message.author))
+  async def on_message(self, message):
+    if (message.content.lower().startswith(prefix + 'startarena')):
+      print("Lo sbatti che ho")
+      curr = conn.cursor()
+      curr.execute(u'''
+      INSERT INTO arenas (author, time)
+      VALUES (?, ?)
+      ''', (str(message.author), dt.datetime.now()))
+      print("Ok, inserted {} into database".format(message.author))
 
 client = MyClient()
 client.run(secret.token)
