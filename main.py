ef35f2e2a3e6b8f7e5c79cab0ddd0dfee2dc70f9
StackWolfed <stackwolfed@protonmail.com>
2019-04-20 15:46:56 +0200

    To solve async function in threading.Timer

--- main.py
+++ main.py
@@ -14,26 +14,47 @@ conn.commit()
 conn.close()
 
 prefix = 'w.'
+serverName = "Sargasso - Wolf SSBU"
 arenaChatsCatName = 'arena chats'
 
 # Function called every 20 seconds to check if any arena has to be deleted. It gets first called on Discord's on_ready function
-def checkDatabaseForDelete():
-  conn2 = sqlite3.connect('example.db')
-  timeUp = dt.datetime.now() - dt.timedelta(hours=1, minutes=0) # timeout arenas
-  curr = conn2.cursor()
-  curr.execute("SELECT * FROM arenas WHERE time < ?", (timeUp.strftime('%Y%m%d%H%M%S'), ))
-  results = curr.fetchall() # returns an array of tuples with the arenas to delete
-  # TODO: Delete the arenas channels and their database entry
-  curr.close()
-  conn2.close()
-  print(results)
-  threading.Timer(20.0, checkDatabaseForDelete).start()
+# def checkDatabaseForDelete():
+#   conn2 = sqlite3.connect('example.db')
+#   timeUp = dt.datetime.now() - dt.timedelta(hours=1, minutes=0) # timeout arenas
+#   curr = conn2.cursor()
+#   curr.execute("SELECT * FROM arenas WHERE time < ?", (timeUp.strftime('%Y%m%d%H%M%S'), ))
+#   results = curr.fetchall() # returns an array of tuples with the arenas to delete
+#   # TODO: Delete the arenas channels and their database entry
+#   curr.close()
+#   conn2.close()
+#   print(results)
+#   threading.Timer(20.0, checkDatabaseForDelete).start()
 
 class MyClient(discord.Client):
+  # Method called every 20 seconds to check if any arena has to be deleted. It gets first called on Discord's on_ready function
+  async def checkDatabaseForDelete(self):
+    conn2 = sqlite3.connect('example.db')
+    timeUp = dt.datetime.now() - dt.timedelta(hours=0, minutes=1) # timeout arenas
+    curr = conn2.cursor()
+    curr.execute("SELECT * FROM arenas WHERE time < ?", (timeUp.strftime('%Y%m%d%H%M%S'), ))
+    results = curr.fetchall() # returns an array of tuples with the arenas to delete
+    curr.close()
+    conn2.close()
+    print(results)
+    threading.Timer(20.0, self.checkDatabaseForDelete).start() # TODO: Solve function to await in Timer
+    if results != []: # Do it only if its needed
+      for entry in self.guilds:
+        if serverName in entry.name:
+          # Found server to delete the channel from: entry.categories
+          for channel in entry.channels:
+            for toDelete in results:
+              if toDelete[1].lower().replace(' ', '-') in channel.name:
+                await channel.delete()
+
   ####################################### on_ready #################################################
   async def on_ready(self):
     print('Logged on as {0}!'.format(self.user))
-    checkDatabaseForDelete()
+    await self.checkDatabaseForDelete()
 
   ###################################### on_message ################################################
   async def on_message(self, message):
@@ -47,7 +68,7 @@ class MyClient(discord.Client):
         curr.execute('''
           INSERT INTO arenas (author, time)
           VALUES (?, ?)
-        ''', (str(message.author), int(dt.datetime.now().strftime('%Y%m%d%H%M%S')))) # Insert into table with automatic ID, author nick and timestamp as '20190420130059'
+        ''', (str(authorName), int(dt.datetime.now().strftime('%Y%m%d%H%M%S')))) # Insert into table with automatic ID, author nick and timestamp as '20190420130059'
         print("Ok, inserted {} into database".format(authorName))
         # Get the 'arena chats' category
         for entry in message.guild.categories:
