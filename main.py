dbdc2c83522686682833a6ce8a3b8784b3a92ad5
StackWolfed <stackwolfed@protonmail.com>
2019-04-20 12:48:44 +0200

    at least it now adds the test

--- main.py
+++ main.py
@@ -8,11 +8,14 @@ import datetime as dt
 conn = sqlite3.connect('example.db')
 
 cursor = conn.cursor()
-cursor.execute("CREATE TABLE IF NOT EXISTS arenas (id INTEGER PRIMARY KEY AUTOINCREMENT, author TEXT, time NUMBER)")
+cursor.execute("CREATE TABLE IF NOT EXISTS arenas (id INTEGER PRIMARY KEY AUTOINCREMENT, author TEXT, time INTEGER)")
 
 cursor.execute('''
 INSERT INTO arenas (author, time) VALUES ('test', 1234)
 ''')
+cursor.close()
+conn.commit()
+conn.close()
 
 prefix = 'w.'
 
@@ -23,6 +26,7 @@ def checkDatabaseForDelete():
   curr.execute("SELECT * FROM arenas") # WHERE time < ?", (timeUp.strftime('%Y%m%d%H%M%S'), )
   results = curr.fetchone()
   curr.close()
+  conn2.close()
   print(results)
   threading.Timer(20.0, checkDatabaseForDelete).start()
 
@@ -34,12 +38,17 @@ class MyClient(discord.Client):
   async def on_message(self, message):
     if (message.content.lower().startswith(prefix + 'startarena')):
       print("Lo sbatti che ho")
+      # SQL STUFF
+      conn = sqlite3.connect('example.db')
       curr = conn.cursor()
       curr.execute('''
       INSERT INTO arenas (author, time)
       VALUES (?, ?)
       ''', (str(message.author), int(dt.datetime.now().strftime('%Y%m%d%H%M%S'))))
       curr.close()
+      conn.commit()
+      conn.close()
+      # END SQL STUFF
       print("Ok, inserted {} into database".format(message.author))
 
 client = MyClient()
