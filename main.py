00567b44c0ae946afd54ead998deec2b176defc3
StackWolfed <stackwolfed@protonmail.com>
2019-04-20 12:22:25 +0200

    using cur

--- main.py
+++ main.py
@@ -5,19 +5,24 @@ import sqlite3
 import threading
 import datetime as dt
 
-conn = sqlite3.connect('example.db', detect_types=sqlite3.PARSE_DECLTYPES)
+conn = sqlite3.connect('example.db')
 
-conn.execute("CREATE TABLE IF NOT EXISTS arenas (id INTEGER PRIMARY KEY AUTOINCREMENT, author TEXT, time DATETIME DEFAULT CURRENT_TIMESTAMP)")
+cursor = conn.cursor()
+cursor.execute("CREATE TABLE IF NOT EXISTS arenas (id INTEGER PRIMARY KEY AUTOINCREMENT, author TEXT, time NUMBER)")
+
+cursor.execute('''
+INSERT INTO arenas (author, time) VALUES ('test', 1234)
+''')
 
 prefix = 'w.'
 
 def checkDatabaseForDelete():
-  conn2 = sqlite3.connect('example.db', detect_types=sqlite3.PARSE_DECLTYPES)
+  conn2 = sqlite3.connect('example.db')
   timeUp = dt.datetime.now() - dt.timedelta(hours=0, minutes=1)
-  print(timeUp)
   curr = conn2.cursor()
-  curr.execute("SELECT * FROM arenas WHERE time < ?", (timeUp, ))
-  results = curr.fetchall()
+  curr.execute("SELECT * FROM arenas") # WHERE time < ?", (timeUp.strftime('%Y%m%d%H%M%S'), )
+  results = curr.fetchone()
+  curr.close()
   print(results)
   threading.Timer(20.0, checkDatabaseForDelete).start()
 
@@ -30,10 +35,11 @@ class MyClient(discord.Client):
     if (message.content.lower().startswith(prefix + 'startarena')):
       print("Lo sbatti che ho")
       curr = conn.cursor()
-      curr.execute(u'''
+      curr.execute('''
       INSERT INTO arenas (author, time)
       VALUES (?, ?)
-      ''', (str(message.author), dt.datetime.now()))
+      ''', (str(message.author), int(dt.datetime.now().strftime('%Y%m%d%H%M%S'))))
+      curr.close()
       print("Ok, inserted {} into database".format(message.author))
 
 client = MyClient()
