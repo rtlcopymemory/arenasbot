1ab0cf504dadd03f747b4c23c4682c47b25ce2c5
StackWolfed <stackwolfed@protonmail.com>
2019-12-30 22:44:07 +0100

    Adds setup and tests with 3.8 Python

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='arenasbot',
+    version='1.0',
+    packages=[''],
+    url='https://github.com/StackWolfed/arenasbot',
+    license='MIT',
+    author='StackWolfed',
+    author_email='stackwolfed@gmail.com',
+    description='Discord Bot to manage temporary arena channels'
+)
